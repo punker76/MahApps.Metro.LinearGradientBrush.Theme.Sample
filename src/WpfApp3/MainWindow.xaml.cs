3eb92bcc5d7e4278c3270892ccb1101d52ac9a75
punker76 <punkerat76@gmail.com>
2019-05-29 22:12:12 +0200

    Add sample app for MahApps issue #3463

---
+++ src/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+
+namespace WpfApp3
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void BlueOnWhite(object sender, RoutedEventArgs args)
+      => ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Cobalt"), ThemeManager.GetAppTheme("BaseLight"));
+
+    private void RedOnBlack(object sender, RoutedEventArgs args)
+      => ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Crimson"), ThemeManager.GetAppTheme("BaseDark"));
+  }
+}
